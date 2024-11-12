f67e2980a3c8f97e4615ff395589bbb562c40f33
legiesh <legiesh@gmail.com>
2024-11-12 11:23:32 +0000

    added app changes and styling

--- social-network-frontend/src/components/SocialForm.tsx
+++ social-network-frontend/src/components/SocialForm.tsx
@@ -23,12 +23,23 @@ const SocialForm: React.FC<SocialFormProps> = ({ onUnconnectedPeopleSubmit, onSo
     };
 
     return (
-        <div>
+        <div className="card p-3 shadow-sm">
             <form>
-                <input type="text" placeholder="Enter person name" value={person} onChange={(e) => setPerson(e.target.value)} />
-                <input type="text" placeholder="Enter networks name" value={networks} onChange={(e) => setNetworks(e.target.value)} />
-                <button type="button" onClick={(e) => handleSubmit(e, 'unconnected')}>Check unconnected people</button>
-                <button type="button" onClick={(e) => handleSubmit(e, "influence")}>Check social influence</button>
+                <div className="row g-2 mb-3">
+                    <div className="col-12 col-md-3">
+                        <input className="w-100 p-1 rounded custom-input" type="text" placeholder="Enter person name" value={person} onChange={(e) => setPerson(e.target.value)} />
+                    </div>
+                    <div className="col-12 col-md-3">
+                        <input className="w-100 p-1 rounded custom-input" type="text" placeholder="Enter networks name" value={networks} onChange={(e) => setNetworks(e.target.value)} />
+                    </div>
+                    <div className="col-12 col-md-3">
+                        
+                        <button type="button" className="btn btn-primary w-100" onClick={(e) => handleSubmit(e, 'unconnected')}>Check unconnected people</button>
+                    </div>
+                    <div className="col-12 col-md-3">                        
+                        <button type="button" className="btn btn-primary w-100" onClick={(e) => handleSubmit(e, "influence")}>Check social influence</button>                        
+                    </div>
+                </div>
             </form>
         </div>
     )
