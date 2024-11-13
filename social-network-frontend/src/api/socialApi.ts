55a1af5320a23f584558509535de61214f3ace32
legiesh <legiesh@gmail.com>
2024-11-13 00:41:02 +0000

    updated env frontend

--- social-network-frontend/src/api/socialApi.ts
+++ social-network-frontend/src/api/socialApi.ts
@@ -4,6 +4,7 @@ import { SocialInfluenceResponse } from '../types/types';
 const  API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 
 export const getUnconnectedPeople  = async(person: string, networks: string[]): Promise<{unconnectedCount: number}> => {
+    console.log("API_BASE_URL:", API_BASE_URL);
     const response = await axios.post(`${API_BASE_URL}/api/unconnectedPeople`, {person, networks})
     return response.data;
 }
